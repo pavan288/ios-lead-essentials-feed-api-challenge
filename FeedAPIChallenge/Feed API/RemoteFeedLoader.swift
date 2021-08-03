8a8fbd71aade4d385b99198eb572c0d7ffcbd2b0
Pavan Powani <pavan.powani@ectosense.com>
2021-08-03 15:39:26 +0530

    Return invalid data error when client responds with invalid data

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -21,10 +21,8 @@ public final class RemoteFeedLoader: FeedLoader {
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
 		client.get(from: url) { result in
 			switch result {
-			case let .success((_, response)):
-				guard response.statusCode == 200 else {
-					return completion(.failure(Error.invalidData))
-				}
+			case .success:
+				completion(.failure(Error.invalidData))
 			case .failure:
 				completion(.failure(Error.connectivity))
 			}
