da80aef1214bec0168cbb56db1756afa5ddc531f
Pavan Powani <pavan.powani@ectosense.com>
2021-08-03 16:23:57 +0530

    Capture self weekly in load method so that the reference to the SUT is not maintained after deallocation (memory leak check)

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -19,7 +19,8 @@ public final class RemoteFeedLoader: FeedLoader {
 	}
 
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
-		client.get(from: url) { result in
+		client.get(from: url) { [weak self] result in
+			guard self != nil else { return }
 			switch result {
 			case let .success((data, response)):
 				if response.statusCode == 200, let images = try? JSONDecoder().decode(ImagesRoot.self, from: data) {
